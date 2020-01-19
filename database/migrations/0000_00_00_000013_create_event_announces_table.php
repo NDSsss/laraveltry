2f83f6354bb128697178e067f6603369ae1f8866
NDS <dddddd1997@gmail.com>
2020-01-20 01:22:32 +0300

    add event and lesson announces to base

--- database/migrations/0000_00_00_000013_create_event_announces_table.php
+++ database/migrations/0000_00_00_000013_create_event_announces_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGroupsTable extends Migration
+class CreateEventAnnouncesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('groups', function (Blueprint $table) {
+        Schema::create('event_announces', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->unique();
-            $table->timestamp('first_lesson_time')->nullable();
-            $table->timestamp('second_lesson_time')->nullable();
+            $table->string('name');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date')->nullable();
             $table->string('address')->nullable();
+            $table->string('extra_info')->nullable();
+            $table->string('is_active')->default(true);
 
             $table->timestamps();
         });
@@ -31,6 +33,6 @@ class CreateGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('event_announces');
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
          $this->call(LessonsTeachersSeeder::class);
          $this->call(TicketsSeeder::class);
          $this->call(StudentsTicketTypesLessonsLeftSeeder::class);
+         $this->call(EventAnnounceSeeder::class);
+         $this->call(LessonAnnounceSeeder::class);
     }
 }
